98b6ba0a0cf17d532287016e9a610898acde26da
GregoryDhm <gregorydehame.etudes@gmail.com>
2024-10-17 16:05:12 +0200

    navigations

---
+++ app/src/main/java/fr/ccm2/projetm2prgd/ui/screen/MainScreen.kt
@@ -0,0 +1,108 @@
+package fr.ccm2.projetm2prgd.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import fr.ccm2.projetm2prgd.R
+
+@Composable
+fun MainScreen (
+    onButtonLoginClick: () -> Unit,
+    onButtonCountriesClick: () -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Ajout de texte avec un style plus travaillé
+            Text(
+                text = "Grégory Dehame",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                ),
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = "Pascal Rohart",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp
+                ),
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Spacer(modifier = Modifier.size(24.dp)) // Ajout d'un espacement entre les éléments
+
+            // Premier bouton stylisé
+            Button(
+                onClick = { onButtonLoginClick() },
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .size(200.dp, 50.dp),
+                shape = RoundedCornerShape(8.dp),
+                contentPadding = PaddingValues(12.dp)
+            ) {
+                Text(
+                    text = LocalContext.current.getString(R.string.login),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White
+                )
+            }
+
+            // Deuxième bouton stylisé
+            Button(
+                onClick = { onButtonCountriesClick() },
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+                    .size(200.dp, 50.dp),
+                shape = RoundedCornerShape(8.dp),
+                contentPadding = PaddingValues(12.dp)
+            ) {
+                Text(
+                    text = LocalContext.current.getString(R.string.Countries),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen(
+        onButtonLoginClick = {},
+        onButtonCountriesClick = {}
+    )
+}
---
+++ app/src/main/java/fr/ccm2/projetm2prgd/ui/screen/RegisterScreen.kt
@@ -0,0 +1,38 @@
+package fr.ccm2.projetm2prgd.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen(
+    navController: NavController,
+){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("LOGIN") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        Column {
+            Text("SCREEN")
+        }
+    }
+}
